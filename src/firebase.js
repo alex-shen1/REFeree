421540c9db0419ab6334c8fc83d6639c0df56d39
Alex Shen <as5gd@viginia.edu>
2020-06-02 11:08:25 -0400

    Set up firebase w/ hidden key

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// this code is copypasted from another thing i did
+import firebase from "firebase/app";
+//these imports are what enable the function calls auth() and database()
+import "firebase/auth"; 
+import "firebase/database"
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    authDomain: "referee-42ffa.firebaseapp.com",
+    databaseURL: "https://referee-42ffa.firebaseio.com",
+    projectId: "referee-42ffa",
+    storageBucket: "referee-42ffa.appspot.com",
+    messagingSenderId: "527189579972",
+    appId: "1:527189579972:web:74e964b67554da94d15191"
+  };
+
+firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth(); // i think this line doesn't do anything
+export default firebase;
