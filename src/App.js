12540d9aa593052b08c81b6b7489216ce0fa1b28
Alex Shen <shen.alexander@gmail.com>
2020-06-02 15:38:50 -0400

    Implemented Google login

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import HomePage from "./Components/HomePage"
 import Login from "./Components/Login"
 
 import Refer from './Components/Refer.js';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+
+
+import firebase, { auth, googleProvider } from "./firebase"
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -15,33 +18,80 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeUser: null // UID of current user
+      activeUser: null, // UID of current user
+      isLoggedIn: false
     }
   }
 
+  componentDidMount() {
+    this.loadUserData(); // run in case user is logged in, but page refreshed
+  }
   setActiveUser = (user) => {
     this.setState({ activeUser: user })
+  }
+
+  handleGoogleLogin = () => {
+    auth.signInWithPopup(googleProvider);
+    auth.getRedirectResult().then(result => {
+      // idk what to do with this
+    }).catch(error => {
+      console.log(error);
+    })
+
+    this.loadUserData()
+  }
+
+  loadUserData = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        console.log(user)
+        this.setState({ activeUser: user.uid, isLoggedIn: true })
+      } else {
+        // console.log("no user")
+      }
+    });
+  }
+
+  handleLogout = () => {
+    console.log("logging out")
+    this.setState({ isLoggedIn: false, activeUser: null })
+    auth.signOut()
+  }
+  test = () => {
+    console.log(this.state.activeUser)
   }
 
   render() {
 
     return (
-      <Router>
-        <div className="app">
+      <div className="app">
+        <Router>
           <Switch>
-            <Route exact path="/" render={() => {
-              return (
-                this.state.activeUser ?
-                  <Redirect to='/home' /> :
-                  <Redirect to='/login' />
-              )
+            <Route exact path="/" render={props => {
+              console.log(this)
+              console.log(this.state)
+              console.log(this.state.isLoggedIn)
+              console.log(props);
+              return this.state.isLoggedIn ?
+                <Redirect to='/home' /> :
+                <Redirect to='/login' />
             }} />
-            <Rooute exact path="/home" component={HomePage} />
+            <Route exact path="/home" component={HomePage} />
             <Route exact path="/FAQ" component={FAQ} />
-            <Route exact path="/login" render={Login} />} />
+            <Route exact path="/login" render={(props) =>
+              <Login {...props}
+                handleGoogleLogin={this.handleGoogleLogin}
+                isLoggedIn={this.state.isLoggedIn}
+                handleLogout={this.handleLogout} />} />
           </Switch>
-        </div>
-      </Router>
+        </Router>
+
+        {/* <button onClick={this.handleGoogleLogin}>Log in w/ google</button>
+        <button onClick={this.loadUserData}>Print user</button>
+        <button onClick={()=>auth.signOut()}>Sign out</button>
+            <button onClick={this.test}>TEST</button> */}
+      </div>
+
     )
   }
 }
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,14 +2,9 @@ import React from 'react';
 import '../Styling/Login.css';
 
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-const firebaseAppAuth = firebaseApp.auth();
-const providers = {
-    googleProvider: new firebase.auth.GoogleAuthProvider(),
-};
 
-class Login extends React.Component {
+export default class Login extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -17,37 +12,22 @@ class Login extends React.Component {
         }
     }
 
-    handleLogin = () => {
-        this.props.signInWithGoogle();
-        console.log(this.props.user.uid)
-    }
-
     render() {
-        const {
-            user,
-            signOut,
-            signInWithGoogle,
-        } = this.props;
         return (
             <div className="App">
                 <header className="App-header">
                     {
-                        user
-                            ? <p>Hello, {user.displayName}</p>
+                        this.props.isLoggedIn
+                            ? <p>Hello, "NAME""</p>
                             : <p>Please sign in.</p>
                     }
                     {
-                        user
-                            ? <button onClick={signOut}>Sign out</button>
-                            : <button onClick={this.handleLo}>Sign in with Google</button>
+                        this.props.isLoggedIn
+                            ? <button onClick={this.props.handleLogout} >Sign out</button>
+                            : <button onClick={this.props.handleGoogleLogin}>Sign in with Google</button>
                     }
                 </header>
             </div>
         )
     }
 }
-
-export default withFirebaseAuth({
-    providers,
-    firebaseAppAuth,
-})(Login);
