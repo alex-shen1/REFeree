ab9f0f3411410123d665540dde8367bd40c5ce0c
Alex Shen <shen.alexander@gmail.com>
2020-06-03 16:16:51 -0400

    Displays referrer's name in account creation

--- src/Components/ReferralLanding.js
+++ src/Components/ReferralLanding.js
@@ -4,10 +4,30 @@ import { database } from "../firebase"
 import Login from "./Login"
 
 export default class ReferralLanding extends Component {
-
+constructor(props){
+    super(props);
+    this.state = {
+        referrersName: ""
+    }
+}
+    componentDidMount = () => {
+        database.ref(`userData/${this.props.id}`).once("value", snapshot=>{
+            if(snapshot && snapshot.exists()){
+                this.setState({referrersName: snapshot.val().name});
+            }
+        })
+    }
+    // getReferrersName = () => {
+    //     database.ref(`userData/${this.props.id}`).once("value", snapshot=>{
+    //         if(snapshot && snapshot.exists()){
+    //             this.setState({referrersName: snapshot.val().name});
+    //         }
+    //     })
+    // }
     render() {
         return <div>
-            {this.props.id}
+            Referred by:
+            {this.state.referrersName}
             <Login
                 isLoggedIn={this.props.isLoggedIn}
                 referrer={this.props.id}
