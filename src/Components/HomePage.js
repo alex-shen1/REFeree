cf81dcdb71023a4c819e61b6d360a975d7d63089
sft3hy <sft3hy@virginia.edu>
2020-06-04 16:12:43 -0400

    Co-authored-by: Esther Chung <e.chung059@gmail.com> Co-authored-by: Alan Li <alanlicmc@gmail.com> Co-authored-by: Alex Shen <shen.alexander@gmail.com>

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -3,7 +3,7 @@ import Progress from "./Progress";
 import Refer from "./Refer";
 import NavPanel from './NavPanel';
 
-import React, { Component } from "react";
+import React from "react";
 
 import { Redirect } from "react-router-dom"
 
@@ -12,7 +12,7 @@ export default function HomePage(props) {
         props.isLoggedIn ?
             <div className="homepage">
                 <NavPanel activeUser={props.activeUser} setActiveUser={props.setActiveUser} />
-                <img src="https://live.staticflickr.com/65535/49970893633_179aa50974_h.jpg" />
+
                 <Refer activeUser={props.activeUser} />
                 <Progress activeUser={props.activeUser} />
             </div> : <Redirect to='/login' />
